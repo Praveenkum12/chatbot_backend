dd967226659c5759adec08640607fc9882a0beb0
PRAVEENKUMAR K <praveenkum1225@gmail.com>
2025-12-21 10:35:35 +0530

    tavily added

--- chatbot/src/main/java/com/jimmy/chatbot/config/OpenAIConfig.java
+++ chatbot/src/main/java/com/jimmy/chatbot/config/OpenAIConfig.java
@@ -5,6 +5,8 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.api.Advisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.ai.rag.advisor.RetrievalAugmentationAdvisor;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +17,13 @@ import java.util.List;
 
 @Configuration
 public class OpenAIConfig {
+
+
+    @Bean
+    ChatMemory chatMemory(JdbcChatMemoryRepository jdbcChatMemoryRepository) {
+        return MessageWindowChatMemory.builder().maxMessages(10)
+                .chatMemoryRepository(jdbcChatMemoryRepository).build();
+    }
 
     @Bean("casualChatClient")
     public ChatClient openaiChatClient(OpenAiChatModel chatModel) {
