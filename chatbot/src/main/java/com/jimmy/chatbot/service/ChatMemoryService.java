ba51f66ba214d457efc34e93e40f381083e2c7ef
PRAVEENKUMAR K <praveenkum1225@gmail.com>
2025-12-21 19:34:15 +0530

    api for chats by id done

--- chatbot/src/main/java/com/jimmy/chatbot/service/ChatMemoryService.java
+++ chatbot/src/main/java/com/jimmy/chatbot/service/ChatMemoryService.java
@@ -5,7 +5,6 @@ import com.jimmy.chatbot.repository.ChatMemoryDetailsRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
